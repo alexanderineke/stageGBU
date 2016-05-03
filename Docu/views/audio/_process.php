708a44c8ae3eec68c2c1689fa696370257e20137
degroot7 <S1076591@student.windesheim.nl>
2016-05-03 15:03:29 +0200

    dat

---
+++ Docu/views/audio/_process.php
@@ -0,0 +1,18 @@
+<?php
+
+$form = ActiveForm::begin([
+            'id' => 'audio-form',
+            'action' => ['audio/process'],
+            'enableAjaxValidation' => false,
+            'method' => 'get',
+            //?↓
+            'options' => 'form',
+                //
+        ]);
+?>
+
+<p class="help-block">Velden met een <span class="required">*</span> zijn verplicht.</p>
+
+<?php echo $form->errorSummary($model); ?>
+
+<?php echo $form->field($model, 'title', array('class'=>'span5','maxlength'=>64)); ?>
