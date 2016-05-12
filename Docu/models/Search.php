04a3a58f79a9c392da7c97ab61f908a0e5fb037a
degroot7 <S1076591@student.windesheim.nl>
2016-05-12 15:17:07 +0200

    SearchModel

--- Docu/models/Search.php
+++ Docu/models/Search.php
@@ -94,56 +94,216 @@ class Search {
             'pages' => $pages
         ];
     }
-/*
-    public function searchDocuments($params) {
-        $query = Document::find()
-                ->joinWith(['tags' => function($q) {
-                $q->where('tags.slug LIKE "%' . $this->tag . '%"');
-            }]);
-        return $dataProvider;
-    }
-*/
 
-    public function searchDocumentsByTag($documentModel, $query) {
-        $q = DocumentTag::find()
-                ->with('tags');
+    /*
+      public function searchDocuments($params) {
+      $query = Document::find()
+      ->joinWith(['tags' => function($q) {
+      $q->where('tags.slug LIKE "%' . $this->tag . '%"');
+      }]);
+      return $dataProvider;
+      }
+     */
 
-        $dataProvider = new ActiveDataProvider([
-            'query' => $q,
-            'pagination' => ['pageSize' => 25],
-            'sort' => [
-                'attributes' => [
-                    'tag_search' => [
-                        'asc' => ['tags.slug' => SORT_ASC],
-                        'desc' => ['tags.slug' => SORT_DESC],
-                    ],
-                    '*',
-                ],
-            ],
-        ]);
+    public function searchDocumentsByTag($params) {
+        $query = DocumentTag::find()
+                ->joinWith('tags')
+                ->andWhere('or', ['like', 'tags.slug', $params['tags.slug'] . '%', true])
+                ->andWhere([
+                    ['like', 'title', $params['title'] . '%', true],
+                    ['like', 'description', $params['description'] . '%', true],
+                    ['like', 'tags.slug', $params['tags.slug'] . '%', true],
+                    ['like', 'year', $params['year'] . '%', true],
+                    ['like', 'tags.state', 1 . '%', false],
+                ])
+                ->groupBy('id');
 
-        $q
-        ->where(['and', ['tags.slug = :tags.slug', [':tags.slug' => $query]]]);
-    }
-
-    public function searchAudio($audioModel, $query) {
-        
-    }
-
-    public function searchAudioByTag($audioModel, $query) {
-        
+        $countQuery = clone $query;
+        $pages = new Pagination(['totalCount' => $countQuery->count(), 'defaultPageSize' => 25]);
+        $datas = $query->offset($pages->offset)
+                ->limit($pages->limit)
+                ->all();
+        return [
+            'datas' => $datas,
+            'pages' => $pages
+        ];
     }
 
-    public function searchImages($query) {
-        
+    public function searchAudio($params) {
+        $query = Audio::find()
+                ->joinWith('tags')
+                ->andWhere([
+                    'or',
+                    ['like', 'content', $params['content'] . '%', true],
+                    ['like', 'description', $params['description'] . '%', true],
+                    ['like', 'year', $params['year'] . '%', true],
+                    ['like', 'title', $params['title'] . '%', true],
+                    ['like', 'tags.slug', $params['tags.slug'] . '%', true],
+                ])
+                ->andWhere([
+                    ['like', 'title', $params['title'] . '%', true],
+                    ['like', 'description', $params['description'] . '%', true],
+                    ['like', 'tags.slug', $params['tags.slug'] . '%', true],
+                    ['like', 'year', $params['year'] . '%', true],
+                    ['like', 'tags.state', 1 . '%', false],
+                ])
+                ->groupBy('id');
+
+        $countQuery = clone $query;
+        $pages = new Pagination(['totalCount' => $countQuery->count(), 'defaultPageSize' => 25]);
+        $datas = $query->offset($pages->offset)
+                ->limit($pages->limit)
+                ->all();
+        return [
+            'datas' => $datas,
+            'pages' => $pages
+        ];
     }
 
-    public function searchImagesByTag($query) {
-        
+    public function searchAudioByTag($params) {
+        $query = AudioTag::find()
+                ->joinWith('tags')
+                ->andWhere([
+                    'or',
+                    ['like', 'content', $params['content'] . '%', true],
+                    ['like', 'description', $params['description'] . '%', true],
+                    ['like', 'year', $params['year'] . '%', true],
+                    ['like', 'title', $params['title'] . '%', true],
+                    ['like', 'tags.slug', $params['tags.slug'] . '%', true],
+                ])
+                ->andWhere([
+                    ['like', 'title', $params['title'] . '%', true],
+                    ['like', 'description', $params['description'] . '%', true],
+                    ['like', 'tags.slug', $params['tags.slug'] . '%', true],
+                    ['like', 'year', $params['year'] . '%', true],
+                    ['like', 'tags.state', 1 . '%', false],
+                ])
+                ->groupBy('id');
+
+        $countQuery = clone $query;
+        $pages = new Pagination(['totalCount' => $countQuery->count(), 'defaultPageSize' => 25]);
+        $datas = $query->offset($pages->offset)
+                ->limit($pages->limit)
+                ->all();
+        return [
+            'datas' => $datas,
+            'pages' => $pages
+        ];
+    }
+
+    public function searchImages($params) {
+        $query = Image::find()
+                ->joinWith('tags')
+                ->joinWith('images')
+                ->andWhere([
+                    'or',
+                    ['like', 'content', $params['content'] . '%', true],
+                    ['like', 'description', $params['description'] . '%', true],
+                    ['like', 'year', $params['year'] . '%', true],
+                    ['like', 'title', $params['title'] . '%', true],
+                    ['like', 'tags.slug', $params['tags.slug'] . '%', true],
+                ])
+                ->andWhere([
+                    ['like', 'title', $params['title'] . '%', true],
+                    ['like', 'description', $params['description'] . '%', true],
+                    ['like', 'tags.slug', $params['tags.slug'] . '%', true],
+                    ['like', 'year', $params['year'] . '%', true],
+                    ['like', 'tags.state', 1 . '%', false],
+                ])
+                ->groupBy('id');
+
+        $countQuery = clone $query;
+        $pages = new Pagination(['totalCount' => $countQuery->count(), 'defaultPageSize' => 25]);
+        $datas = $query->offset($pages->offset)
+                ->limit($pages->limit)
+                ->all();
+        return [
+            'datas' => $datas,
+            'pages' => $pages
+        ];
+    }
+
+    public function searchImagesByTag($params) {
+        $query = ImageTag::find()
+                ->joinWith('tags')
+                ->joinWith('images')
+                ->andWhere([
+                    'or',
+                    ['like', 'content', $params['content'] . '%', true],
+                    ['like', 'description', $params['description'] . '%', true],
+                    ['like', 'year', $params['year'] . '%', true],
+                    ['like', 'title', $params['title'] . '%', true],
+                    ['like', 'tags.slug', $params['tags.slug'] . '%', true],
+                ])
+                ->andWhere([
+                    ['like', 'title', $params['title'] . '%', true],
+                    ['like', 'description', $params['description'] . '%', true],
+                    ['like', 'tags.slug', $params['tags.slug'] . '%', true],
+                    ['like', 'year', $params['year'] . '%', true],
+                    ['like', 'tags.state', 1 . '%', false],
+                ])
+                ->groupBy('id');
+
+        $countQuery = clone $query;
+        $pages = new Pagination(['totalCount' => $countQuery->count(), 'defaultPageSize' => 25]);
+        $datas = $query->offset($pages->offset)
+                ->limit($pages->limit)
+                ->all();
+        return [
+            'datas' => $datas,
+            'pages' => $pages
+        ];
     }
 
     public function popularTags() {
+        $most_popular_tags_sql = 'SELECT tag_id, magnitude
+            FROM (
+                SELECT * FROM 
+                (
+                    SELECT tag_id, COUNT(*) AS magnitude 
+                    FROM tbl_document_tag
+                    GROUP BY tag_id 
+                    ORDER BY magnitude DESC
+                    LIMIT 10
+                ) popular_document_tags
+
+                UNION ALL
+
+                SELECT * FROM
+                ( 
+                    SELECT tag_id, COUNT(*) AS magnitude 
+                    FROM tbl_image_tag
+                    GROUP BY tag_id 
+                    ORDER BY magnitude DESC
+                    LIMIT 10
+                ) popular_image_tags
+
+                UNION ALL
+
+                SELECT * FROM
+                ( 
+                    SELECT tag_id, COUNT(*) AS magnitude 
+                    FROM tbl_audio_tag
+                    GROUP BY tag_id 
+                    ORDER BY magnitude DESC
+                    LIMIT 10
+                ) popular_audio_tags
+            ) top_results
+        GROUP BY tag_id
+        ORDER BY magnitude DESC
+        LIMIT 10';
         
+        $tag_ids = [];
+        if($result = \Yii::$app->db->createCommand($most_popular_tags_sql)->query()){
+            foreach($result as $row){
+                $tag_ids[] = $row['tag_ids'];
+            }
+            $tagModel = new Tag();
+            $tags = $tagModel->findTagsByID($tag_ids, true);
+        }
+        if($tags){
+            return $tags;
+        }
     }
 
 }
