8ecf95f5c5fe686033a3579413cffe55c67e073e
degroot7 <S1076591@student.windesheim.nl>
2016-05-20 15:00:01 +0200

    image index

--- Docu/views/image/index.php
+++ Docu/views/image/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\widgets\Menu;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\Search */
@@ -9,41 +10,43 @@ use yii\grid\GridView;
 
 $this->title = 'Afbeeldingen';
 $this->params['breadcrumbs'][] = $this->title;
-        
-$this->params['menu'][] = [
+
+echo Menu::widget([
+    'items' => [
         ['label' => 'Acties', 'visible' => Yii::$app->user->getIdentity('moderator')],
         ['label' => 'Maak afbeeldingen aan', 'url' => ['create'], 'icon' => 'file', 'visible' => Yii::$app->user->getIdentity('user')],
         ['label' => 'Beheer afbeeldingen', 'url' => ['admin'], 'icon' => 'list-alt', 'visible' => Yii::$app->user->getIdentity('admin')],
-   ];
+    ],
+]);
 ?>
 <div class="image-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+<?php // echo $this->render('_search', ['model' => $searchModel]);  ?>
 
     <p>
-        <?= Html::a('Create Image', ['create'], ['class' => 'btn btn-success']) ?>
+<?= Html::a('Create Image', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?=
-    GridView::widget([
-        'id' => 'image-grid',
-       // 'type' => 'striped bordered',
-        'dataProvider' => $model->search(),
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            ['label' => 'images', 'format' => 'html', //'htmlOptions' => [
-                   // 'style' => 'width: 100px; text-align: center;',
-               /*];*/  'header' => 'Voorbeeld'/*, 'value' => 'CHtml::image("uploads/afbeeldingen/".$data->images[0]->location."/thumb/".$data->images[0]->file.$data->images[0]->format)'*/],
-            ['label' => 'title', 'header' => 'Titel', 'format' => 'html',/* 'value' => 'CHtml::link($data->title, Yii::app()->createUrl("image/view",["id"=>$data->id)])'*/],
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-      //  'enableHistory' => true,
-        'pager' => [
-            'prevPageLabel' => '&laquo;',
-            'nextPageLabel' => '&raquo;',
-        ],
-    ]);
-    ?>
+<?=
+GridView::widget([
+    'id' => 'image-grid',
+    // 'type' => 'striped bordered',
+    'dataProvider' => $model->search(),
+    'columns' => [
+        ['class' => 'yii\grid\SerialColumn'],
+        ['label' => 'images', 'format' => 'html', //'htmlOptions' => [
+            // 'style' => 'width: 100px; text-align: center;',
+            /* ]; */ 'header' => 'Voorbeeld'/* , 'value' => 'CHtml::image("uploads/afbeeldingen/".$data->images[0]->location."/thumb/".$data->images[0]->file.$data->images[0]->format)' */],
+        ['label' => 'title', 'header' => 'Titel', 'format' => 'html', /* 'value' => 'CHtml::link($data->title, Yii::app()->createUrl("image/view",["id"=>$data->id)])' */],
+        ['class' => 'yii\grid\ActionColumn'],
+    ],
+    //  'enableHistory' => true,
+    'pager' => [
+        'prevPageLabel' => '&laquo;',
+        'nextPageLabel' => '&raquo;',
+    ],
+]);
+?>
 
 </div>
